9caaedf42b5c10fca10afe7e60bf82080a6aaf70
tonyl59 <tony.luong59@yahoo.com>
2025-05-12 15:01:36 +0200

    exo51

---
+++ src/main/java/org/example/Exercice51.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Exercice51 {
+    public static void main(String[] args) {
+        /*1.
+            Créer une variable nom et une variable prenom
+          2.
+            Afficher la phrase suivante "Bonjour {prenom} {NOM}."
+          3.
+            Remplacer les valeurs entre chevrons par les variables créées précédemment */
+
+        String nom,prenom;
+        nom = "toto";
+        prenom = "tata";
+        System.out.println("Bonjour " + prenom + " "+nom);
+
+    }
+}
